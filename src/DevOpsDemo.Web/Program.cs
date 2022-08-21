272e417f737d71b3211aee27fec16bb2fd071048
AndyLeung9527 <407833635@qq.com>
2022-08-21 15:53:44 +0800

    去掉顶级语句

--- src/DevOpsDemo.Web/Program.cs
+++ src/DevOpsDemo.Web/Program.cs
@@ -1,6 +1,15 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+namespace DevOpsDemo.Web
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+            app.MapGet("/", () => "Hello World!");
 
-app.Run();
+            app.Run();
+        }
+    }
+}
